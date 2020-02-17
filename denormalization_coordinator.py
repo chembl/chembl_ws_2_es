894eca8c821ae878d01c6b9d67b40baaa0208602
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-02-17 14:07:28 +0000

    fix package path

--- denormalization_coordinator.py
+++ denormalization_coordinator.py
@@ -5,5 +5,5 @@ SCRIPT_DIR = os.path.abspath(os.path.join(SCRIPT_PATH, os.pardir))
 GLADOS_ES_PATH = os.path.join(SCRIPT_DIR, 'src')
 sys.path.append(GLADOS_ES_PATH)
 
-import glados.ws2es.denormalization.denormalization_coordinator as denormalization_coordinator
+import glados.es.ws2es.denormalization.denormalization_coordinator as denormalization_coordinator
 denormalization_coordinator.main()
--- migration_handler.py
+++ migration_handler.py
@@ -5,5 +5,5 @@ SCRIPT_DIR = os.path.abspath(os.path.join(SCRIPT_PATH, os.pardir))
 GLADOS_ES_PATH = os.path.join(SCRIPT_DIR, 'src')
 sys.path.append(GLADOS_ES_PATH)
 
-import glados.ws2es.migration_handler as migration_handler
+import glados.es.ws2es.migration_handler as migration_handler
 migration_handler.main()
