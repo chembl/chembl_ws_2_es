2779b65bc3691ffa4ab21ebb0c0cf6e157d82cfb
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-04-06 23:14:10 +0100

    initial version for testing long id lists in elastic v7

---
+++ src/glados/es/ws2es/test/test_long_id_list_denormalization.py
@@ -0,0 +1,10 @@
+import glados.es.ws2es.es_util as es_util
+import unittest
+import logging
+__author__ = 'jfmosquera@ebi.ac.uk'
+
+logging_level = logging.INFO
+
+
+class TestCase(unittest.TestCase):
+    pass
