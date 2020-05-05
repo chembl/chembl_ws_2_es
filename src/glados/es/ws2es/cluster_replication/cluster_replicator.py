22e77aa2f29fb15f95184c6098dc55a0abca13f6
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-05-05 19:58:27 +0100

    replicator first version

--- src/glados/es/ws2es/cluster_replication/cluster_replicator.py
+++ src/glados/es/ws2es/cluster_replication/cluster_replicator.py
@@ -2,18 +2,54 @@ import argparse
 import sys
 import time
 from datetime import datetime, timedelta
-import glados.es.ws2es.es_util as es_util
+from glados.es.ws2es.es_util import ESUtil
 import glados.es.ws2es.signal_handler as signal_handler
+import glados.es.ws2es.resources_description as resources_description
+import glados.es.ws2es.progress_bar_handler as pbh
+from threading import Thread
+import traceback
 
 __author__ = 'jfmosquera@ebi.ac.uk'
-
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Replication
 # ----------------------------------------------------------------------------------------------------------------------
 
-def replicate_clusters(cluster_connection_origin, cluster_connection_2):
-    pass
+
+class IndexReplicator(Thread):
+
+    def __init__(self, idx_name: str, es_util_origin: ESUtil, es_util_dest: ESUtil):
+        super().__init__()
+        self.idx_name = idx_name
+        self.es_util_origin = es_util_origin
+        self.es_util_dest = es_util_dest
+
+    def replicate_idx(self):
+        self.es_util_dest.delete_idx(self.idx_name)
+        self.es_util_dest.create_idx(self.idx_name, mappings=self.es_util_origin.get_index_mapping(self.idx_name))
+        print('INFO: Index created for {0}.'.format(self.idx_name), file=sys.stderr)
+        sys.stderr.flush()
+
+        def index_doc_on_doc(scan_doc, scan_doc_id, total_docs, current_count, firts_doc, last_doc):
+            self.es_util_dest.index_doc_bulk(self.idx_name, scan_doc_id, scan_doc)
+
+        self.es_util_origin.scan_index(self.idx_name, index_doc_on_doc)
+
+    def run(self):
+        try:
+            self.replicate_idx()
+        except:
+            traceback.print_exc()
+
+
+def replicate_clusters(es_util_origin: ESUtil, es_util_dest: ESUtil):
+    replicators = []
+    for resource_i in resources_description.ALL_RESOURCES:
+        res_it_i = IndexReplicator(resource_i.idx_name, es_util_origin, es_util_dest)
+        res_it_i.start()
+        replicators.append(res_it_i)
+    for res_it_i in replicators:
+        res_it_i.join()
 
 # ----------------------------------------------------------------------------------------------------------------------
 # MAIN
@@ -22,45 +58,75 @@ def replicate_clusters(cluster_connection_origin, cluster_connection_2):
 
 def main():
     t_ini = time.time()
-    parser = argparse.ArgumentParser(description="Denormalize ChEMBL data existing in Elastic Search")
-    parser.add_argument("--host",
-                        dest="es_host",
-                        help="Elastic Search Hostname or IP address.",
+    parser = argparse.ArgumentParser(
+        description="Replicate ChEMBL data existing in Elastic Search from origin to destination."
+    )
+    parser.add_argument("--host-origin",
+                        dest="es_host_origin",
+                        help="Elastic Search Hostname or IP address for origin cluster.",
                         default="localhost")
-    parser.add_argument("--user",
-                        dest="es_user",
-                        help="Elastic Search username.",
+    parser.add_argument("--user-origin",
+                        dest="es_user_origin",
+                        help="Elastic Search username for origin cluster.",
                         default=None)
-    parser.add_argument("--password",
-                        dest="es_password",
-                        help="Elastic Search username password.",
+    parser.add_argument("--password-origin",
+                        dest="es_password_origin",
+                        help="Elastic Search username password for origin cluster.",
                         default=None)
-    parser.add_argument("--port",
-                        dest="es_port",
-                        help="Elastic Search port.",
+    parser.add_argument("--port-origin",
+                        dest="es_port_origin",
+                        help="Elastic Search port for origin cluster.",
                         default=9200)
-    parser.add_argument("--unichem",
-                        dest="denormalize_unichem",
-                        help="If included will denormalize the unichem related data.",
-                        action="store_true",)
-    parser.add_argument("--activity",
-                        dest="denormalize_activity",
-                        help="If included will denormalize the configured activity related data.",
-                        action="store_true",)
-    parser.add_argument("--compound_hierarchy",
-                        dest="denormalize_compound_hierarchy",
-                        help="If included will denormalize the Compound Hierarchy data.",
-                        action="store_true",)
-    parser.add_argument("--mechanism_and_drug_indication",
-                        dest="denormalize_mechanism_and_drug_indication",
-                        help="If included will denormalize the Mechanism and Drug Indication data.",
-                        action="store_true",)
+    parser.add_argument("--host-destination",
+                        dest="es_host_destination",
+                        help="Elastic Search Hostname or IP address for destination cluster.",
+                        default="localhost")
+    parser.add_argument("--user-destination",
+                        dest="es_user_destination",
+                        help="Elastic Search username for destination cluster.",
+                        default=None)
+    parser.add_argument("--password-destination",
+                        dest="es_password_destination",
+                        help="Elastic Search username password for destination cluster.",
+                        default=None)
+    parser.add_argument("--port-destination",
+                        dest="es_port_destination",
+                        help="Elastic Search port for destination cluster.",
+                        default=9200)
     args = parser.parse_args()
 
-    es_util.setup_connection(args.es_host, args.es_port, args.es_user, args.es_password)
-    es_util.bulk_submitter.start()
+    es_util_origin = ESUtil()
+    es_util_origin.setup_connection(
+      args.es_host_origin, args.es_port_origin, args.es_user_origin, args.es_password_origin
+    )
+    es_util_destination = ESUtil()
+    es_util_destination.setup_connection(
+      args.es_host_destination, args.es_port_destination, args.es_user_destination, args.es_password_destination
+    )
 
-    signal_handler.add_termination_handler(es_util.stop_scan)
+    ping_failed = False
+
+    if not es_util_origin.ping():
+        print('ERROR: Ping failed to origin cluster.', file=sys.stderr)
+        ping_failed = True
+
+    if not es_util_destination.ping():
+        print('ERROR: Ping failed to destination cluster.', file=sys.stderr)
+        ping_failed = True
+
+    if ping_failed:
+        return
+
+    es_util_destination.bulk_submitter.start()
+
+    signal_handler.add_termination_handler(es_util_origin.stop_scan)
+    signal_handler.add_termination_handler(es_util_destination.stop_scan)
+    signal_handler.add_termination_handler(es_util_destination.bulk_submitter.stop_submitter)
+
+    replicate_clusters(es_util_origin, es_util_destination)
+
+    es_util_destination.bulk_submitter.join()
+    pbh.write_after_progress_bars()
 
     end_msg = 'REPLICATION FINISHED'
 
