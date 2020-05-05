591017b7b9097e284e536a1c34af14fcc3ef1d50
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-05-05 20:24:02 +0100

    relocation of query_yes_no

--- src/glados/es/ws2es/migration_handler.py
+++ src/glados/es/ws2es/migration_handler.py
@@ -4,6 +4,7 @@ import time
 from datetime import datetime, timedelta
 from glados.es.ws2es.es_util import es_util
 from glados.es.ws2es.resource_iterator import ResourceIterator, SharedThreadPool
+from glados.es.ws2es.util import query_yes_no
 import glados.es.ws2es.resources_description as resources_description
 import glados.es.ws2es.progress_bar_handler
 import glados.es.ws2es.migration_common as migration_common
@@ -14,40 +15,6 @@ __author__ = 'jfmosquera@ebi.ac.uk'
 # ----------------------------------------------------------------------------------------------------------------------
 # MAIN
 # ----------------------------------------------------------------------------------------------------------------------
-
-
-def query_yes_no(question, default="yes"):
-    """
-    Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
 
 
 def main():
--- src/glados/es/ws2es/util.py
+++ src/glados/es/ws2es/util.py
@@ -6,6 +6,7 @@ import time
 from wrapt.decorators import synchronized
 import re
 from concurrent.futures import Future
+import sys
 
 PROPERTY_REGEX = re.compile('[0-9A-Za-z_]*')
 PROPERTY_NAME_IDS = {}
@@ -272,3 +273,37 @@ class SharedThreadPool(Thread):
                 self.check_finished_tasks()
             else:
                 time.sleep(0.2)
+
+
+def query_yes_no(question, default="yes"):
+    """
+    Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
