e63dd8b37c8484775dfe88dba8c871a7bdc9c730
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-05-27 19:11:47 +0100

    initial definition of compound structural alerts mappings

--- src/glados/es/ws2es/mappings_skeletons/es_chembl_compound_structural_alert_mapping.py
+++ src/glados/es/ws2es/mappings_skeletons/es_chembl_compound_structural_alert_mapping.py
@@ -3,54 +3,54 @@ from glados.es.ws2es.es_util import DefaultMappings
 
 # Shards size - can be overridden from the default calculated value here
 # shards = 3,
-replicas = 0
+replicas = 1
 
 analysis = DefaultMappings.COMMON_ANALYSIS
 
 mappings = \
     {
-        'properties':
+        'properties': 
         {
-            'alert':
+            'alert': 
             {
-                'properties':
+                'properties': 
                 {
-                    'alert_id': DefaultMappings.ID_REF,
+                    'alert_id': 'NUMERIC',
                     # EXAMPLES:
                     # '1' , '1' , '1' , '1' , '1' , '1' , '1' , '1' , '1' , '1'
-                    'alert_name': DefaultMappings.LOWER_CASE_KEYWORD,
+                    'alert_name': 'TEXT',
                     # EXAMPLES:
                     # 'R1 Reactive alkyl halides' , 'R1 Reactive alkyl halides' , 'R1 Reactive alkyl halides' , 'R1 Reac
                     # tive alkyl halides' , 'R1 Reactive alkyl halides' , 'R1 Reactive alkyl halides' , 'R1 Reactive alk
                     # yl halides' , 'R1 Reactive alkyl halides' , 'R1 Reactive alkyl halides' , 'R1 Reactive alkyl halid
                     # es'
-                    'alert_set':
+                    'alert_set': 
                     {
-                        'properties':
+                        'properties': 
                         {
-                            'priority': DefaultMappings.INTEGER,
+                            'priority': 'NUMERIC',
                             # EXAMPLES:
                             # '8' , '8' , '8' , '8' , '8' , '8' , '8' , '8' , '8' , '8'
-                            'set_name': DefaultMappings.LOWER_CASE_KEYWORD,
+                            'set_name': 'TEXT',
                             # EXAMPLES:
-                            # 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' ,
+                            # 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 'Glaxo' , 
                             # 'Glaxo'
                         }
                     },
-                    'smarts': DefaultMappings.KEYWORD,
+                    'smarts': 'TEXT',
                     # EXAMPLES:
                     # '[Br,Cl,I][CX4;CH,CH2]' , '[Br,Cl,I][CX4;CH,CH2]' , '[Br,Cl,I][CX4;CH,CH2]' , '[Br,Cl,I][CX4;CH,CH
                     # 2]' , '[Br,Cl,I][CX4;CH,CH2]' , '[Br,Cl,I][CX4;CH,CH2]' , '[Br,Cl,I][CX4;CH,CH2]' , '[Br,Cl,I][CX4
                     # ;CH,CH2]' , '[Br,Cl,I][CX4;CH,CH2]' , '[Br,Cl,I][CX4;CH,CH2]'
                 }
             },
-            'cpd_str_alert_id': DefaultMappings.ID,
+            'cpd_str_alert_id': 'NUMERIC',
             # EXAMPLES:
             # '34544046' , '34544047' , '34544048' , '34544049' , '34544050' , '34544051' , '34544052' , '34544053' , '3
             # 4544054' , '34544055'
-            'molecule_chembl_id': DefaultMappings.CHEMBL_ID_REF,
+            'molecule_chembl_id': 'TEXT',
             # EXAMPLES:
-            # 'CHEMBL2205881' , 'CHEMBL2205882' , 'CHEMBL2206028' , 'CHEMBL2206762' , 'CHEMBL2216877' , 'CHEMBL2219513'
+            # 'CHEMBL2205881' , 'CHEMBL2205882' , 'CHEMBL2206028' , 'CHEMBL2206762' , 'CHEMBL2216877' , 'CHEMBL2219513' 
             # , 'CHEMBL2219607' , 'CHEMBL2219650' , 'CHEMBL2219694' , 'CHEMBL2219698'
         }
     }
